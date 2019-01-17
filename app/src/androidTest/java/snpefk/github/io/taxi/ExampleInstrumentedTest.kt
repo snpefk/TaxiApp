290905748f1b516ce6b236b1d0d52323e71ed05e
snpefk <snpefk@gmail.com>
2019-01-17 15:41:45 +0300

    Init commit

---
+++ app/src/androidTest/java/snpefk/github/io/taxi/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package snpefk.github.io.taxi
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("snpefk.github.io.taxi", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/snpefk/github/io/taxi/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package snpefk.github.io.taxi.presentation.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import snpefk.github.io.taxi.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
