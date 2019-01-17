25b4ba6a943cb2ba624017866dc6604de2cbdebb
snpefk <snpefk@gmail.com>
2019-01-17 19:38:12 +0300

    Network: фикс десериадизации LocalDateTime

---
+++ app/src/main/java/snpefk/github/io/taxi/data/network/deserializer/LocalDateTimeDeserializer.kt
@@ -0,0 +1,13 @@
+package snpefk.github.io.taxi.data.network.deserializer
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.ZonedDateTime
+import java.lang.reflect.Type
+
+class LocalDateTimeDeserializer : JsonDeserializer<LocalDateTime> {
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): LocalDateTime =
+        ZonedDateTime.parse(json.asString).toLocalDateTime()
+}
