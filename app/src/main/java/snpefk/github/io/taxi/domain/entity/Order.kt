45fe46925808afc0e09d2a20051c47ea581a74d4
snpefk <snpefk@gmail.com>
2019-01-17 15:55:00 +0300

    Order: добавил свойство dateTime

--- app/src/main/java/snpefk/github/io/taxi/domain/entity/Order.kt
+++ app/src/main/java/snpefk/github/io/taxi/domain/entity/Order.kt
@@ -1,9 +1,12 @@
 package snpefk.github.io.taxi.domain.entity
 
+import org.threeten.bp.LocalDateTime
+
 class Order(
     val id: Long,
     val startAddress: Address,
     val endAddress: Address,
     val price: Price,
+    val dateTime: LocalDateTime,
     val vehicle: Vehicle
 )
